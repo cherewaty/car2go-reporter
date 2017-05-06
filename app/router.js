4e74b9b4144a74e8a1545bf7bb1976ff1bc64725
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2017-05-06 11:59:27 -0500

    Create vehicle route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('vehicle', { path: '/vehicles/:vehicle_id' });
 });
 
 export default Router;
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('vehicle');
+  },
+
+  actions: {
+    handleVehicleClick(vehicleId) {
+      this.transitionTo('vehicle', vehicleId);
+    }
   }
 });
