dfbb05ff0ece3c6054ac315e150d9c4984605b87
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2017-05-07 12:06:59 -0500

    Wire up report action

--- app/components/vehicle-report.js
+++ app/components/vehicle-report.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    submitReport(reportType) {
+      this.get('onSubmitReport')(this.get('vehicle'), reportType);
+    }
+  }
 });
--- app/routes/vehicle.js
+++ app/routes/vehicle.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').peekRecord('vehicle', params.vehicle_id);
+  },
+
+  actions: {
+    sendReport(vehicle) {
+      this.transitionTo('vehicle.report-success', vehicle.get('id'));
+    }
   }
 });
